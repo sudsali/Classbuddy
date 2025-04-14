2e747439a6b93a511fbe762b11c6c578d2aba44b
Lingfeng Fan <fanlingfengcq@gmail.com>
2025-04-14 16:17:43 -0400

    implemented chat room feature

--- backend/apps/study_groups/urls.py
+++ backend/apps/study_groups/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudyGroupViewSet
+from .views import StudyGroupViewSet, ChatMessageViewSet
 
 router = DefaultRouter()
 router.register('', StudyGroupViewSet, basename='study-group')
+router.register('messages', ChatMessageViewSet, basename='message')
 
 urlpatterns = [
     path('', include(router.urls)),
