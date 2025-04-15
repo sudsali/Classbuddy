baeb1c51a4e7b578a7a9f993e45610f6505fe7ba
rachit <pathakrachit4@gmail.com>
2025-04-15 03:08:30 -0400

    Implement email verification system

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
 
 class StudyGroup(models.Model):
--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from django.utils.crypto import get_random_string
+from django.core.mail import send_mail
 
 User = get_user_model()
 
@@ -28,11 +30,26 @@ class RegisterSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
+        validated_data.pop('password2')
+        verification_code = get_random_string(length=6, allowed_chars='0123456789')
+        
         user = User.objects.create(
             email=validated_data['email'],
             first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            last_name=validated_data['last_name'],
+            is_verified=False,
+            verification_code=verification_code
         )
         user.set_password(validated_data['password'])
         user.save()
-        return user 
+
+        # Send email with the code
+        send_mail(
+        subject="Verify your email",
+        message=f"Your verification code is: {verification_code}",
+        from_email=None,  # Uses DEFAULT_FROM_EMAIL
+        recipient_list=[user.email],
+        fail_silently=False,  # <--- So we get error messages if it fails
+        )
+
+        return user
