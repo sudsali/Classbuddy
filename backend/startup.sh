fa9a33c5755e93f60a268d5aa35397ac9a3bb7a1
Sudhanshu Sali <sudsali10@gmail.com>
2025-05-22 18:14:56 -0700

    Fixing issue

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py migrate
 gunicorn classbuddy.wsgi:application --bind=0.0.0.0:8000
